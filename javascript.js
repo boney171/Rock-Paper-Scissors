b4fd97a68f901801ac7bdd9b9b6e178fe29af8b2
Tri Tran <ttran456@ucr.edu>
2022-06-29 13:45:24 -0700

    Finished implementing logic functions

--- javascript.js
+++ javascript.js
@@ -10,41 +10,85 @@ function computerPlay(){
     }
     return 'scissor';
 }
-
 function oneRound(playerSel, computerSel){
-   let playerSelection = playerSel.toLowerCase();
-   let computerSelection = computerSel.toLowerCase();
-   if(playerSelection == 'rock' && computerSelection == 'rock'){
-      return "Rock and Rock, It's a draw!";
-   }
-   else if(playerSelection == 'paper' && computerSelection == 'paper'){
-      return "Paper and Paper, It's a draw!";
-   }
-   else if(playerSelection == 'scissor' && computerSelection == 'scissor'){
-      return "Scissor and Scissor, It's a draw!";
-   }
-   else if(playerSelection == 'rock' && computerSelection == 'paper'){
-      return "You Lose! Paper beats Rock!";
-   }
-   else if(playerSelection == 'rock' && computerSelection == 'scissor'){
-      return "You Win! Rock beats scissor!";
-   }
-   else if(playerSelection == 'paper' && computerSelection == 'rock'){
-      return "You Win! Paper beats Rock!";
-   }
-   else if(playerSelection == 'paper' && computerSelection == 'scissor'){
-      return "You Lose! Scissor beats paper!";
-   }
-   else if(playerSelection == 'scissor' && computerSelection == 'rock'){
-      return "You Lose! Rock beats Scissor!";
-   }
-   else if(playerSelection == 'scissor' && computerSelection == 'paper'){
-      return "You Win! Scissor beats Paper!";
-   }
+  // 3 cases:
+   let result = "";
+  //Draw cases
+   if((playerSel == 'rock' && computerSel == 'rock') || (playerSel == 'paper' && computerSel == 'paper')
+  ||(playerSel == 'scissor' && computerSel == 'scissor')){
+      result = 'draw';
+  }
+   else if((playerSel == 'rock' && computerSel == 'scissor') || (playerSel == 'paper' && computerSel == 'rock')
+  ||(playerSel == 'scissor' && computerSel == 'paper')){
+      result = 'win';
+  }
+   else if((playerSel == 'scissor' && computerSel == 'rock') || (playerSel == 'rock' && computerSel == 'paper')
+  ||(playerSel == 'paper' && computerSel == 'scissor')){
+      result = 'lose';
+  }
+
+  return result;
 }
 
-for(let i = 0; i < 5; i++){
-   let input = prompt("Enter a selection");
-   let computerSelection = computerPlay();
-   document.write(oneRound(input,computerSelection));
-}
+
+let result = '';
+let playerScore = 0;
+let computerScore = 0;
+let computerSelection = '';
+const pScore = document.querySelector('.pScore');
+const cScore = document.querySelector('.cScore');
+const story = document.querySelector('h4');
+const btn = document.querySelectorAll('button');
+
+btn.forEach((button) => {
+
+   button.addEventListener('click', () => {
+      let userChoose = button.id;
+      let computerChoose = computerPlay();
+      result = oneRound(button.id, computerChoose);
+      if(playerScore == 5){
+         alert("Congratulation, you won! Refresh to play again!");
+         window.location.reload();
+      }
+      if(computerScore == 5){
+         alert("Better luck next time! Refresh to play again!");
+         window.location.reload();
+      }
+      if(result == 'win'){
+         playerScore++;
+         pScore.innerHTML = playerScore;
+         story.innerHTML = `Good job! ${userChoose} beats ${computerChoose}, Pick again!`;
+      }
+      else if(result == 'lose'){
+         computerScore++;
+         cScore.innerHTML = computerScore;
+         story.innerHTML = `Sorry! ${computerChoose} beats ${userChoose}, Try again!`;
+      } else {
+         story.innerHTML = `It's a draw! ${computerChoose} and ${userChoose}, Try again!`;
+      }
+    
+      
+
+   });
+});
+
+
+
+//Test function
+  
+ 
+   
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
